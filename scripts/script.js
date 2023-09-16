1c70b71c1435570e2137ba5cfb783a18a3d3d7fa
Santiago <santiagoanmucha@gmail.com>
2023-09-15 22:12:33 -0500

    corregiuendo error en la api

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,56 @@
+const url = "http://localhost:5000/api/products";
+const responseDiv = document.getElementById("response");
+document.addEventListener("DOMContentLoaded", function() {
+    const responseDiv = document.getElementById("response");
+  
+    if (responseDiv) {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data); // Imprime la respuesta completa en la consola para inspección
+  
+          if (Array.isArray(data.products) && data.products.nodes.length > 0) {
+            const products = data.products;
+            const productHTML = products.map((product) => {
 
-const url ="http://localhost:5000/api/products";
-function getProducts() {
-    fetch('url') // La URL de la API
-        .then(response => response.json())
-        .then(data => {
-            const productList = document.getElementById('product-list');
-            productList.innerHTML = ''; // Limpiar contenido anterior
-            data.products.forEach(product => {
-                productList.innerHTML += `
-                    <div>
-                        <strong>ID:</strong> ${product[0]}<br>
-                        <strong>Nombre:</strong> ${product[1]}<br>
-                        <strong>Categoría:</strong> ${product[2]}<br>
-                        <strong>Precio:</strong> ${product[3]}<br>
-                        <strong>Valor:</strong> ${product[4]}<br>
-                        <strong>Stock:</strong> ${product[5]}<br><br>
-                    </div>
-                `;
-            });
+
+
+
+
+
+
+// Renderiza el promedio y el número de estrellas
+
+//fin del sistema
+
+//fin regla
+
+              return `
+              <div class="product-card">
+              <div class="product-image">
+            
+                  <img src="${product.featuredImage.url}" class="product-thumb" alt="">
+                  <button class="card-btn">add to cart</button>
+              </div>
+              <div class="product-info">
+                  <h2 class="product-brand">${product.title}</h2>
+                  <span class="stars"></span><span class="tags">()</span>
+                  <span class="price"> ${product.prices.max.amount} </span><span class="actual-price"></span>
+
+              </div>
+          </div>
+              `;
+            }).join('');
+  
+            responseDiv.innerHTML = productHTML;
+          } else {
+            console.error("No se encontraron elementos para mostrar.");
+          }
         })
-        .catch(error => console.error('Error:', error));
-}
-
-// Llamar a la función para obtener la lista de productos al cargar la página
-getProducts();
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      console.error("El elemento con el ID 'response' no se encontró en el documento.");
+    }
+  });
