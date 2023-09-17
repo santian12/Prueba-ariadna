d7fbb9ef33dc673437c51912b31d151e65825953
Santiago <santiagoanmucha@gmail.com>
2023-09-16 19:27:49 -0500

    creacion crud

--- scripts/script.js
+++ scripts/script.js
@@ -13,26 +13,52 @@ document.addEventListener("DOMContentLoaded", function () {
                     const products = data.products;
 
                     if (products.length > 0) {
-                        const productHTML = products.map((product) => {
+                        const productRows = products.map((product) => {
                             return `
-                                <div class="container">
-                                    <div class="columntr">
-                                        <h2>ID: ${product.id}</h2>
-                                        <p>Nombre: ${product.nombre}</p>
-                                    </div>
-                                    <div class="columntr">
-                                        <h2>Categoría: ${product.categoria}</h2>
-                                        <p>Precio: $${product.precio}</p>
-                                    </div>
-                                    <div class="columntr">
-                                        <h2>Valor: $${product.valor}</h2>
-                                        <p>Stock: ${product.stock}</p>
-                                    </div>
-                                </div>
+                                <tr>
+                                    <td>${product.id}</td>
+                                    <td>${product.nombre}</td>
+                                    <td>${product.categoria}</td>
+                                    <td>$${product.precio}</td>
+                                    <td>$${product.valor}</td>
+                                    <td>${product.stock}</td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <span>Mouse over me</span>
+                                            <div class="dropdown-content">
+                                            <button id="addProductButton">Añadir Producto</button>
+                                                <p>Actualiza el producto</p>
+                                                <p>Eliminar el producto</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
                             `;
                         }).join('');
 
-                        responseDiv.innerHTML = productHTML;
+                        // Crear la tabla y agregar las filas generadas
+                        const tableHTML = `
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nombre</th>
+                                            <th>Categoría</th>
+                                            <th>Precio</th>
+                                            <th>Valor</th>
+                                            <th>Stock</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        ${productRows}
+                                    </tbody>
+                                </table>
+                            </div>
+                        `;
+
+                        responseDiv.innerHTML = tableHTML;
                     } else {
                         responseDiv.innerHTML = "No se encontraron elementos para mostrar.";
                     }
@@ -47,3 +73,54 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("El elemento con el ID 'response' no se encontró en el documento.");
     }
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const createProductForm = document.getElementById("createProductForm");
+
+  createProductForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const formData = new FormData(createProductForm);
+  
+      // Crear un objeto con los datos del formulario
+      const newProduct = {
+          nombre: formData.get("nombre"),
+          categoria: formData.get("categoria"),
+          precio: parseFloat(formData.get("precio")),
+          valor: parseFloat(formData.get("valor")),
+          stock: parseInt(formData.get("stock")),
+      };
+  
+      // Aquí puedes realizar una solicitud POST para crear el producto en tu API
+      // Puedes usar fetch() nuevamente, similar a como lo hiciste para obtener la lista de productos
+      // Asegúrate de manejar la respuesta de la API y, si es exitosa, agregar el nuevo producto a tu tabla
+  
+      // Ejemplo (debes adaptarlo a tu API):
+      fetch(url, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newProduct),
+      })
+      .then((response) => response.json())
+      .then((data) => {
+          // Actualiza la tabla con el nuevo producto si es exitoso
+          if (data.message === "Producto creado con éxito") {
+              // Agrega el nuevo producto a la tabla existente o vuelve a cargar la página para mostrar los cambios
+              // Puedes implementar esto según tus necesidades específicas
+          }
+      })
+      .catch((error) => {
+          console.error("Error al crear el producto:", error);
+      });
+  });
+  
+});
+
+
+
+
+
+
+
+
